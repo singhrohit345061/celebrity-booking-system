4ab0a2c495ba7e43d8435e40e29b2d650fee1c4b
singhrohit345061 <singhrohit345061@gmail.com>
2026-03-09 15:25:49 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "8055",
-database: "celebrity_booking"
+host: process.env.MYSQLHOST,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE,
+port: process.env.MYSQLPORT
 });
 
 db.connect((err)=>{
 if(err){
-console.log("Database connection failed", err);
+console.log(err);
 }else{
-console.log("MySQL Connected");
+console.log("Database connected");
 }
 });
 
