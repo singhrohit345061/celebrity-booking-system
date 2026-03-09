ab542b9150f7f744a56edbacdc2b90631ab4a3b4
singhrohit345061 <singhrohit345061@gmail.com>
2026-03-09 18:51:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ res.send("Celebrity Booking API Running");
 app.use("/",usersRoute);
 app.use("/",bookingRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
 console.log("Server running on port "+PORT);
